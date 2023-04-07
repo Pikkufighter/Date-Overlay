b35d0d56a881416929e6a50b47423d0705edef36
Pikkufighter <csjholm65@gmail.com>
2023-04-07 10:41:39 +0300

    Additional configurations and optimization

--- src/main/java/com/dateOverlay/DateOverlayConfig.java
+++ src/main/java/com/dateOverlay/DateOverlayConfig.java
@@ -7,12 +7,34 @@ import net.runelite.client.config.ConfigItem;
 @ConfigGroup("DateOverlay")
 public interface DateOverlayConfig extends Config
 {
+	String CONFIG_GROUP = "dateOverlay";
+
 	@ConfigItem(
 			keyName = "showTime",
 			name = "Show Time",
 			description = "Show hours, minutes and seconds?"
 	)
 	default boolean showTime()
+	{
+		return false;
+	}
+
+	@ConfigItem(
+			keyName = "nameMonth",
+			name = "Month name",
+			description = "Show month as text instead of number"
+	)
+	default boolean nameOfMonth()
+	{
+		return false;
+	}
+
+	@ConfigItem(
+			keyName = "shortYear",
+			name = "Short Year",
+			description = "Have year as 2 characters instead of 4"
+	)
+	default boolean shortYear()
 	{
 		return false;
 	}
